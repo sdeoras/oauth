fd26980b8a7934097ffd3e47a0dc88041e85a0cf
Saurabh Deoras <sdeoras@gmail.com>
2019-03-08 08:52:01 -0800

    use info api
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- google.go
+++ google.go
@@ -1,13 +1,17 @@
 package oauth
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/google/uuid"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
 type googleProvider struct {
-	oauthStateString string
+	oauthStateString  string
 	googleOauthConfig *oauth2.Config
 }
 
@@ -27,4 +31,30 @@ func NewGoogleProvider(redirectURL, clientId, clientSecret string) Provider {
 	}
 
 	return g
+}
+
+func (g *googleProvider) GetUserInfo(r *http.Request) ([]byte, error) {
+	state, code := r.FormValue("state"), r.FormValue("code")
+	if state != g.oauthStateString {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+
+	token, err := g.googleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" +
+		token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	b, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	return b, nil
 }
--- interface.go
+++ interface.go
@@ -1,6 +1,9 @@
 package oauth
 
+import "net/http"
+
 // Provider provides a URL to redirect for oauth
 type Provider interface {
 	Url() string
+	GetUserInfo(r *http.Request) ([]byte, error)
 }
